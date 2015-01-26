40139fcf326b4dd09bd7e9f9a3f5a6ac25b68e53
Stian Eikeland <stian@eikeland.se>
2015-01-27 00:20:58 +0100

    added yesql dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive "1.1.5"]])
+                 [enlive "1.1.5"]
+                 [yesql "0.4.0"]
+                 [org.postgresql/postgresql "9.3-1102-jdbc41"]])
