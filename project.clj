a18dff118fd62ee536494b6a3c914ec4d8a44b07
Stian Eikeland <stian@eikeland.se>
2015-01-28 20:51:51 +0100

    added ring, compojure, liberator deps

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[compojure "1.3.1"]
                  [enlive "1.1.5"]
-                 [yesql "0.4.0"]
-                 [org.postgresql/postgresql "9.3-1102-jdbc41"]])
+                 [liberator "0.12.2"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.postgresql/postgresql "9.3-1102-jdbc41"]
+                 [ring/ring-core "1.3.2"]
+                 [yesql "0.4.0"]])
