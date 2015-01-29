7efa9aca6c6dca45fc9aeef0e0c5b47a21f9775d
Stian Eikeland <stian@eikeland.se>
2015-01-30 00:33:11 +0100

    test food resource

--- test/sushi_machine/server_test.clj
+++ test/sushi_machine/server_test.clj
@@ -1,11 +1,24 @@
 (ns sushi-machine.server-test
   (:require [sushi-machine.server :refer :all]
             [clojure.test :refer :all]
+            [clojure.edn :as edn]
             [ring.mock.request :as mock]))
 
 (defmacro req [& args]
-  `(test-handler (mock/request ~@args)))
+  `(-> (mock/request ~@args)
+       (mock/header "accept" "application/edn")
+       (mock/content-type "application/edn")
+       (test-handler)))
 
-(deftest brille-test
-  (is (= (:body (req :get "/"))
-         "This is an example")))
+(deftest food-rest-resource
+
+  (testing "food resource returns list of food items with name and price"
+    (let [res (req :get "/food/isushi")
+          body (edn/read-string (:body res))]
+      (is (> (count body) 10))
+      (is (contains? (first body) :name))
+      (is (contains? (first body) :price))))
+
+  (testing "food resource returns 404 on not found item"
+    (let [res (req :get "/food/doesntexist")]
+      (is (= (:status res) 404)))))
